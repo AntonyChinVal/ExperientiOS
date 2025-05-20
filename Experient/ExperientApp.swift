ceba9ee384e6e22411473ffa0dbdde0689c1ab51
youngvz <vshah@experientgroup.com>
2025-05-20 08:25:52 -0400

    Initial Commit

---
+++ Experient/ExperientApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExperientApp.swift
+//  Experient
+//
+//  Created by youngvz on 5/20/25.
+//
+
+import SwiftUI
+
+@main
+struct ExperientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExperientTests/ExperientTests.swift
@@ -0,0 +1,16 @@
+//
+//  ExperientTests.swift
+//  ExperientTests
+//
+//  Created by youngvz on 5/20/25.
+//
+
+import Testing
+
+struct ExperientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
